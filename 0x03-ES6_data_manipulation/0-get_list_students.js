3abdaa9b247cd43748795545d56719a9bf6c2150
abdulnasirym <abdulnasiry05@gmail.com>
2024-10-08 08:56:42 -0700

    function named getListStudents that returns an array of objects.

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,9 @@
+export default function getListStudents() {
+  const arr = [
+    { id: 1, firstname: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstname: 'James', location: 'columbia' },
+    { id: 3, firstname: 'Serena', location: 'San francisco' },
+  ];
+
+  return arr;
+}
