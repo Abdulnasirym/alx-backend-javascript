47666e8cc00daa49fbca69ca23ecc27bcda1ab53
abdulnasirym <abdulnasiry05@gmail.com>
2024-10-08 08:59:45 -0700

    Adjusted spelling in 'Columbia'

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,7 +1,7 @@
 export default function getListStudents() {
   const arr = [
     { id: 1, firstname: 'Guillaume', location: 'San Francisco' },
-    { id: 2, firstname: 'James', location: 'columbia' },
+    { id: 2, firstname: 'James', location: 'Columbia' },
     { id: 3, firstname: 'Serena', location: 'San francisco' },
   ];
 
