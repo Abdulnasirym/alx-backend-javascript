e4db93c53652173cc588f2e14aa030cfedf3deb6
abdulnasirym <abdulnasiry05@gmail.com>
2024-10-11 10:06:55 -0700

    wrote a function and also an interface for printTeacher

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -7,7 +7,15 @@ interface Teacher {
   [key: string]: any;
 }
 
-
 interface Directors extends Teacher {
   numberOfReports: number;
+}
+
+interface printTeacherFunction {
+  firstName: string;
+  lastName: string;
+}
+
+export default function printTeacher(firstName: string, lastName: string): string {
+  return `${firstName[0]}. ${lastName}`;
 }
