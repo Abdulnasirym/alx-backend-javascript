9ba54de62aa86b9400ddf022e4739f2dc58c4430
Abdulnasir Yakubu muhammad <132953212+Abdulnasirym@users.noreply.github.com>
2024-10-22 12:04:08 +0100

    pricing

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,16 +1,32 @@
 import Currency from "./3-currency"
 
 export default class Pricing {
-  constructor(amount: int, currency: currency) {
+  constructor(amount, currency) {
     this._amount = amount;
-    this.currency = currency;
+    this._currency = currency;
   }
 
-   get amount() {
-     return this._amount;
-   }
+  get amount() {
+    return this._amount;
+  }
 
   set amount(value) {
     return this._amount = value;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(value) {
+    return this._currency = value;
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
   }
 }
