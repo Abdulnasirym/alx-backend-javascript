18449088c66c54ee0bd6fb1b33af568bde23abdb
abdulnasirym <abdulnasiry05@gmail.com>
2024-11-26 17:27:40 +0000

    adjsutment 2

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,9 +1,8 @@
 /**
  * Displays a message on the STDOUT.
  * @param {String} msg The message to display.
- * @author Bezaleel Olakunori <https://github.com/B3zaleel>
  */
-function displayMessage(msg){
+function displayMessage(msg) {
   console.log(msg);
 };
 
