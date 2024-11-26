e2a6b8c8633341752c0918485525885b8d664072
abdulnasirym <abdulnasiry05@gmail.com>
2024-11-26 17:25:57 +0000

    Adjustmetn 1

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,6 +1,10 @@
-// Display a message to STDOUT
-function displayMessage(msg) {
-    console.log(msg);
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ * @author Bezaleel Olakunori <https://github.com/B3zaleel>
+ */
+function displayMessage(msg){
+  console.log(msg);
 };
 
 module.exports = displayMessage;
